bb49f7869952e51d784c030c91486c6d86e9eba8
7RU7H <100765038+7RU7H@users.noreply.github.com>
2022-03-03 09:37:26 +0000

    Update bashPingSweep.sh
    Added #!/bin/bash
    Added comment to remind replacement of $IP and its
    Added pseudo variable IP

--- bashPingSweep.sh
+++ bashPingSweep.sh
@@ -1,4 +1,6 @@
-#/bin/bash
+#!/bin/bash
 #bash ping sweep
-for i in {1..255}; do (ping -c 1 192.168.1.${i} | grep "bytes from" &); done
+#replace octets of IP variable
+IP=10.10.10.
+for i in {1..255}; do (ping -c 1 $IP${i} | grep "bytes from" &); done
 
