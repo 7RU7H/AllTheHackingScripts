14d16d584367089fd287a10976bc7a20c23ab9ad
7RU7H <100765038+7RU7H@users.noreply.github.com>
2022-03-03 18:32:02 +0000

    Update userColonPassBase64.sh

--- bash/userColonPassBase64.sh
+++ bash/userColonPassBase64.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 # Concatenates a given "user:wordlist_elem" -> base64 encodes it and outputs it to a file
-USERU="Joker"
-USERL="joker"
+USER="USER"
 DELIM=":"
 LIST=$(cat /usr/share/wordlists/rockyou.txt)
 for WORD in $LIST;  do
-	echo $USERU$DELIM$WORD | base64 >> list.txt
-	echo $USERL$DELIM$WORD | base64 >> list.txt
+	echo $USER$DELIM$WORD | base64 >> list.txt
 done
