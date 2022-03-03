2eabf4fa0f5caf3faa81d9a4dcc515794c121caf
7RU7H <100765038+7RU7H@users.noreply.github.com>
2022-03-03 09:48:33 +0000

    Update urlEncoder.py

--- python3/urlEncoder.py
+++ python3/urlEncoder.py
@@ -1,5 +1,5 @@
 import urllib.parse
-
+# Just a url encoder
 print("Hello welcome to 7RU7H/nvm's urlEncoder script. Please enter a string you wish to encode:\n")
 INPUT = input()
 print(urllib.parse.quote(INPUT))
