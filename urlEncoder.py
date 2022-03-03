79cfa0fccc3714605dfe696a8ae26db1e36049e7
7RU7H <100765038+7RU7H@users.noreply.github.com>
2022-03-03 09:47:50 +0000

    Update urlEncoder.py

--- urlEncoder.py
+++ urlEncoder.py
@@ -1,6 +1,6 @@
 import urllib.parse
 
-print("Hello welcome to nvm's urlEncoder script. Please enter a string you wish to encode:\n")
+print("Hello welcome to 7RU7H/nvm's urlEncoder script. Please enter a string you wish to encode:\n")
 INPUT = input()
 print(urllib.parse.quote(INPUT))
 exit()
