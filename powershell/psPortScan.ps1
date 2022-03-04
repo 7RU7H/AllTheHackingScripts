f34ed3b57f5beae5a075f0c770612eda50355986
7RU7H <100765038+7RU7H@users.noreply.github.com>
2022-03-04 11:03:14 +0000

    Update psPortScan.ps1

--- powershell/psPortScan.ps1
+++ powershell/psPortScan.ps1
@@ -1,8 +1,6 @@
 #Determine IP ranges to scan(in this case it will be localhost) and you can provide the input in any way you want
 #Determine the port ranges to scan
 #Determine the type of scan to run(in this case it will be a simple TCP Connect Scan)
-
-
 $ip = 'localhost'
 $portmin = 0
 $portmax = 1024
