1c8941712d2b156fbe42af3b1702921d50b7b80d
7RU7H <100765038+7RU7H@users.noreply.github.com>
2022-03-05 10:06:28 +0000

    Add files via upload

---
+++ python3/fileDownloader.py
@@ -0,0 +1,6 @@
+# https://tryhackme.com/room/pythonforcybersecurity
+import requests
+
+url = 'https://download.sysinternals.com/files/PSTools.zip'
+r = requests.get(url, allow_redirects=True)
+open('PSTools.zip', 'wb').write(r.content)
---
+++ python3/keylog.py
@@ -0,0 +1,4 @@
+# https://tryhackme.com/room/pythonforcybersecurity
+import keyboard
+keys = keyboard.record(until ='ENTER')
+keyboard.play(keys)
