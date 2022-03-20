3a52828f2a1894d03e1c8ec716329f614f3c9aef
7RU7H <100765038+7RU7H@users.noreply.github.com>
2022-03-20 11:37:33 +0000

    Create detectPSdowngrade.ps1

---
+++ powershell/detectPSdowngrade.ps1
@@ -0,0 +1,7 @@
+Get-WinEvent -LogName "Windows PowerShell" |
+    Where-Object Id -eq 400 |
+    Foreach-Object {
+        $version = [Version] (
+            $_.Message -replace '(?s).*EngineVersion=([\d\.]+)*.*','$1')
+        if($version -lt ([Version] "5.0")) { $_ }
+}
