2b26b0b69d0296c24cd2a6f54d7681e8df6e73e8
kali <mkthun@protonmail.com>
2022-09-05 10:08:34 +0100

    Works but still reports seg faults..

--- bash/AutoNmap.sh
+++ bash/AutoNmap.sh
@@ -7,6 +7,18 @@ if [ "$#" -ne 2 ]; then
 		exit
 fi
 
+if [[ "$EUID" = 0 ]]; then
+    echo "(1) Currently root"
+else
+    sudo -k # make sure to ask for password on next sudo
+    if sudo true; then
+        echo "(2) Correct password"
+    else
+        echo "(3) Incorrect password"
+        exit 1
+    fi
+fi
+
 # Run a port scan and output error, if require -Pn to err file
 sudo nmap -p- -n --min-rate 1000 -oN nmap/allPorts $1 2>&1 > /tmp/nmap-err
 
@@ -25,20 +37,34 @@ open=$(cat $2/nmap/allPorts | grep "open" | grep -v "Discovered" | awk '{print $
 closed=$(cat $2/nmap/allPorts | grep "closed" | grep -v "Discovered" | awk '{print $1}' | awk -F '/' '{print $1}' | tr '\n' ',')
 filtered=$(cat $2/nmap/allPorts | grep "filter" | grep -v "Discovered" | awk '{print $1}' | awk -F '/' '{print $1}' | tr '\n' ',')
 
-echo "Open ports : " $open
-echo "Closed ports : " $closed
+echo "Open ports : " ${open::-1}
+echo "Closed ports : " ${closed::-1}
 echo "Filtered ports : " $filtered
 
+# New substituion variable to prevent race condition for bash to load.. nothing at all
+targetports=${open::-1}
 
-
-sudo nmap $nmap_pn_flag -sC -sV -oA $2/nmap/Extensive --min-rate 1000 -p{open::-1} $1
+sudo nmap $nmap_pn_flag -sC -sV -oA $2/nmap/Extensive --min-rate 1000 -p $targetports $1
 wait
-sudo nmap $nmap_pn_flag -script discovery -oA $2/nmap/Discovery --min-rate 1000 -p{open::-1} $1 
+echo
+echo
+echo
+sudo nmap $nmap_pn_flag -script discovery -oA $2/nmap/Discovery --min-rate 1000 -p $targetports $1 
 wait
-sudo nmap $nmap_pn_flag --script vuln -oA $2/nmap/Vuln --min-rate 1000 -p{open::-1} $1
+echo
+echo
+echo
+sudo nmap $nmap_pn_flag --script vuln -oA $2/nmap/Vuln --min-rate 1000 -p $targetports $1
 wait
+echo
+echo
+echo
 sudo nmap $nmap_pn_flag -sU -oA $2/nmap/UDP --min-rate 1000 -p- $1
 wait
-
+echo
+echo
+echo
+echo "Scans completed"
+exit
 
 
