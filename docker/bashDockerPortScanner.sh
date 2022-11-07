e4e733af5a6a1caa79e799fbcf8b8f5707a4d7cf
7ru7h <mkthun@protonmail.com>
2022-11-07 18:52:12 +0000

    Minor modifications

--- docker/bashDockerPortScanner.sh
+++ docker/bashDockerPortScanner.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # https://tryhackme.com/room/hololive
+default_gateway="" # set to default gateway
 ports=(21 22 53 80 443 3306 8443 8080)   
 for port in ${ports[@]}; do  	
 	timeout 1 
-	bash -c "echo \"Port Scan Test\" > /dev/tcp/1.1.1.1/$port && echo $port is open || /dev/null"    
+	bash -c "echo \"Port Scan Test\" > /dev/tcp/$default_gateway/$port && echo $port is open || /dev/null"    
 done
--- docker/pythonDockerPortScanner.py
+++ docker/pythonDockerPortScanner.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 import socket   
-host = "127.0.0.1"   
+host = "" # Set to default gateway   
 portList = [21,22,53,80,443,3306,8443,8080]   
 for port in portList:
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
