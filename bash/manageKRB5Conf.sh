ffeca589ebe4b7f08f10afce78bde029e1e82f09
truth <mkthun@protonmail.com>
2023-07-24 21:09:21 +0100

    Manage KRB5 Configuration file

---
+++ bash/manageKRB5Conf.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Author: 7ru7h
+
+if [ "$#" -ne 4 ]; then
+        echo "Usage: $0 <cmd: add / remove>/ setup <realm> <kdc> <admin_server>"
+        echo "run \`sudo apt-get install krb5-user\' - put: \`KALI\` as default in all capitals, no \` for adding and removing a default realm"
+        exit
+fi
+
+CMD=$1
+
+function addToKRB5Conf ()
+{
+        REALM=$1
+        KDC=$2
+        ADM=$3
+        echo "Adding: $@"
+        # add realm
+        sudo sed -i 's/default_realm = KALI/default_realm = $REALM/g' /etc/krb5.conf
+        sudo sed -i 's/\n\tKALI = {\n\t\tkdc = KALI\n\t\tadmin_server = KALI\n\t}/\n\t$REALM = {\n\t\tkdc = $KDC\n\t\tadmin_server = $ADM\n\t}/g' /etc/krb5.conf
+        cat /etc/krb5.conf
+        return
+}
+
+function removeFromKRB5Conf ()
+{
+        REALM=$1
+        KDC=$2
+        ADM=$3
+        echo "Remove and replacing back to default KALI every field: $@"
+        sudo sed -i 's/default_realm = $REALM/default_realm = KALI/g' /etc/krb5.conf
+        sudo sed -i 's/\n\t$REALM = {\n\t\tkdc = $KDC\n\t\tadmin_server = $ADM\n\t}/\n\tKALI = {\n\t\tkdc = KALI\n\t\tadmin_server = KALI\n\t}/g' /etc/krb5.conf
+        cat /etc/krb5.conf
+        return
+}
+
+case $CMD in
+        "add") addToKRB5Conf $2 $3 $4 ;;
+        "remove") removeFromKRB5Conf $2 $3 $4 ;;
+        "setup") echo "run \`sudo apt-get install krb5-user\' - put: KALI as default in all capitals for adding and removing a  default realm"
+        *) echo $CMD " is invalid" ;;
+esac
+exit
